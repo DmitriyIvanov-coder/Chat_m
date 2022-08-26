ff1788a6d96779e56c380c9e4cecc6b0bb6197f5
DmitriyR <ivanov.dimok2000@yandex.ru>
2022-08-27 00:19:03 +0300

    some fixes for DB

--- server/src/main/java/org/example/DataBaseClients.java
+++ server/src/main/java/org/example/DataBaseClients.java
@@ -16,17 +16,17 @@ public class DataBaseClients {
     public static void setConnection() throws ClassNotFoundException, SQLException {
         connection = null;
         Class.forName("org.sqlite.JDBC");
-        connection = DriverManager.getConnection("jdbc:sqlite:TEST1.firstDB");
+        connection = DriverManager.getConnection("jdbc:sqlite:RegisteredClients:firstDB");
     }
 
     public static void createDB() throws SQLException {
         statement = connection.createStatement();
         statement.execute("CREATE TABLE if not exists 'clients'"+"('id' INTEGER PRIMARY KEY AUTOINCREMENT," +
-                "'name' text, 'password' text);");
+                "'login' text, 'password' text);");
     }
 
     public void addClient(String login, String password) throws SQLException {
-        try(PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO 'user' ('login', password) VALUES (?,?)")){
+        try(PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO 'clients' ('login', password) VALUES (?,?)")){
             preparedStatement.setString(1,login);
             preparedStatement.setString(2,password);
             preparedStatement.execute();
@@ -35,27 +35,29 @@ public class DataBaseClients {
     }
 
     public  boolean checkExists(String login) throws SQLException {
-        try(PreparedStatement preparedStatement = connection.prepareStatement("SELECT login FROM clients WHERE name = ?")){
+        try(PreparedStatement preparedStatement = connection.prepareStatement("SELECT login FROM clients WHERE login = ?")){
             preparedStatement.setString(1,login);
             resultSet = preparedStatement.executeQuery();
-            if (resultSet.wasNull()){
-                //Если нет совпадений, то всё в порядке
-               return true;
-            }else
+            if (resultSet.next()){
                 return false;
+            }else
+                //Если нет совпадений, то всё в порядке
+                return true;
+
         }
     }
 
     public  boolean checkAuthorization(String login, String password) throws SQLException {
-        try(PreparedStatement preparedStatement = connection.prepareStatement("SELECT login, password FROM clients WHERE name = ? AND password = ?")){
+        try(PreparedStatement preparedStatement = connection.prepareStatement("SELECT login, password FROM clients WHERE login = ? AND password = ?")){
             preparedStatement.setString(1,login);
             preparedStatement.setString(2,password);
             resultSet = preparedStatement.executeQuery();
-            if (resultSet.wasNull()){
+            if (resultSet.next()){
+                return true;
+            }else
                 //Если нет совпадений, то авторизация не должна пройти
                 return false;
-            }else
-                return true;
+
         }
     }
 }
